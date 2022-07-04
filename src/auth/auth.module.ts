ed912fae6961d7b47c9ec5011caf8103cd906e2e
Lidia <in.canaless@gmail.com>
2022-07-03 22:49:34 -0600

    refactor: remove unused strategy

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { JwtModule, JwtService } from '@nestjs/jwt';
+import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { UsersModule } from 'src/users/users.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { jwtConstants } from './constants';
-import { LocalStrategy } from './local.strategy';
 import { SpotifyOauthStrategy } from './strategies/spotify-oauth.strategy';
 import { JwtStrategy } from './strategies/jwt.strategy';
 @Module({
